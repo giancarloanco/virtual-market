5ea898869905cd0352aa50b6dad5b5f9bef531bf
Ruben <42656904+6b726d@users.noreply.github.com>
2020-06-11 19:52:39 -0500

    Update settings.py
    Add lines. (44 and 47).

--- virtual-market-backend/VirtualMarket/VirtualMarket/settings.py
+++ virtual-market-backend/VirtualMarket/VirtualMarket/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
